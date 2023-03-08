f40c4a225b98f7d8e353f58fbda95389ed32e327
cyvu <christofferdahlberg@gmail.com>
2023-03-08 19:52:02 +0100

    db update

---
+++ js/classes/database.js
@@ -0,0 +1,56 @@
+//import { mysql_user, mysql_pass, mysql_db } from '/env/credentials.js';
+let mysql = require('mysql')
+
+class Database
+{
+  constructor() {
+      this.connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'school',
+    })
+  }
+
+  isConnected() {
+    return this.connection.connect((err) => {
+      if (err) {
+        console.error('error during disconnection', err.stack)
+        return reject(err)
+      }
+    })
+  }
+
+  closeConnection() {
+    return this.connection.end((err) => {
+      if (err) {
+        console.error('error during disconnection', err.stack)
+        return reject(err)
+      }
+    })
+  }
+
+  read(table, field) {
+    this.connection.query(`SELECT ${field} FROM ${table}`, function (err, rows, fields) {
+      if (err) throw err
+      if (rows) {
+        process.stdout.write(JSON.stringify( rows ))
+      }
+    })
+  }
+
+  write(table, values) {
+    this.connection.query(`INSERT INTO ${table} VALUES (${values})`, function (err, rows, fields) {
+      if (err) throw err
+      if (rows) {
+        process.stdout.write(JSON.stringify( rows ))
+      }
+    })
+  }
+}
+
+const db = new Database()
+db.isConnected()
+//db.write('persons', `2, "Spring", "Blixted", "Some gata 1", "Höteborg"`)
+db.read('persons', 'LastName')
+db.closeConnection()
