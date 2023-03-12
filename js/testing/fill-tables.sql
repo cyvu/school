54bdb3c09365d3dfd61a8af7030963a1af76c945
cyvu <christofferdahlberg@gmail.com>
2023-03-12 18:55:45 +0100

    table alterations

--- js/testing/fill-tables.sql
+++ js/testing/fill-tables.sql
@@ -1,6 +1,5 @@
-INSERT INTO Users VALUES (
-  "Bengt",
-  "Bengan",
-  "bengtsson@pri.com",
-  CURRENT_TIMESTAMP()
-)
+INSERT INTO users ("firstname", "lastname", "email", "last_logged_in")
+VALUES ('Cyvu', 'leetCoder', 'fakemail@forever.yeah', CURRENT_TIMESTAMP());
+
+INSERT INTO users ("firstname", "lastname", "email", "last_logged_in")
+VALUES ('Xokketeer', 'willKnowEverything', 'fakymai@yeah.forever', CURRENT_TIMESTAMP());
