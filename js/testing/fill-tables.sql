121385686063806b864579f3abbe52a2bbff6cb9
cyvu <christofferdahlberg@gmail.com>
2023-03-31 23:06:43 +0200

    removed old files

--- js/testing/fill-tables.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users ("firstname", "lastname", "email", "last_logged_in")
-VALUES ('Cyvu', 'leetCoder', 'fakemail@forever.yeah', CURRENT_TIMESTAMP());
-
-INSERT INTO users ("firstname", "lastname", "email", "last_logged_in")
-VALUES ('Xokketeer', 'willKnowEverything', 'fakymai@yeah.forever', CURRENT_TIMESTAMP());
